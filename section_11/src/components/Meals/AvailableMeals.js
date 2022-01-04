3489bf80e61a597dc96e3ab8b9f0c73af5eb0310
Won-young <dnjsdud2257@gmail.com>
2022-01-04 16:58:47 +0900

    개발 음식 아이템 컴포넌트 및 공통 UI card 추가

--- section_11/src/components/Meals/AvailableMeals.js
+++ section_11/src/components/Meals/AvailableMeals.js
@@ -1,3 +1,5 @@
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
 
 const DUMMY_MEALS = [
@@ -28,13 +30,23 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeal = () => {
-  const MealLise = DUMMY_MEALS.map((meal) => {
-    return <li>{meal.name}</li>;
+  const mealList = DUMMY_MEALS.map((meal) => {
+    return (
+      <MealItem
+        id={meal.id}
+        key={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+      />
+    );
   });
 
   return (
     <section className={classes.meals}>
-      <ul>{MealLise}</ul>
+      <Card>
+        <ul>{mealList}</ul>
+      </Card>
     </section>
   );
 };
