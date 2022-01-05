7ac22e5e4613f20ad3f516213c2a03e1cfd2cbcd
Won-young <dnjsdud2257@gmail.com>
2022-01-05 19:36:02 +0900

    카트 버튼에 useEffect로 애니메이션 추가

--- section_11/src/components/Layout/HeaderCartButton.js
+++ section_11/src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,40 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHigthlighted] = useState(false);
+
   const cartCtx = useContext(CartContext);
 
   const numberOfCartItem = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const { items } = cartCtx;
+
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnIsHigthlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHigthlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button onClick={props.onClick} className={classes.button}>
+    <button onClick={props.onClick} className={btnClasses}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
