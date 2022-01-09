75965ce3dfc853d18318317447f57daf93acfc36
Won-young <dnjsdud2257@gmail.com>
2022-01-09 22:20:55 +0900

    값을 입력받고 검증 및 피드백 추가

--- section16/src/components/SimpleInput.js
+++ section16/src/components/SimpleInput.js
@@ -1,9 +1,54 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const inputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredNameTouched, setEnteredNametouched] = useState(false);
+
+  const nameInputChangeHandler = (e) => {
+    setEnteredName(e.target.value);
+  };
+
+  const formSubmitissonHandler = (e) => {
+    e.preventDefault();
+
+    setEnteredNametouched(true);
+
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
+
+    setEnteredNameIsValid(true);
+
+    console.log(enteredName);
+    const enteredValue = inputRef.current.value;
+    console.log(enteredValue);
+
+    setEnteredName("");
+  };
+
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  const nameInputClasses = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmitissonHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={inputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
+        {nameInputIsInvalid && (
+          <p className="error-text">이름은 공백이 안됩니다.</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
