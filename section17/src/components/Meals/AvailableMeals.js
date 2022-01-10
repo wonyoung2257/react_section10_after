a962d13dad3b6efd7dae1184ee096e3afcaaeb3c
Won-young <dnjsdud2257@gmail.com>
2022-01-11 00:50:47 +0900

    checkout 추가

--- section17/src/components/Meals/AvailableMeals.js
+++ section17/src/components/Meals/AvailableMeals.js
@@ -6,29 +6,36 @@ import classes from "./AvailableMeals.module.css";
 const AvailableMeals = () => {
   const [mealsArray, setMealsArray] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
   const getMeal = async () => {
-    const response = await fetch(
-      "https://reatc-http-default-rtdb.firebaseio.com/meals.json"
-    );
-
-    if (!response.ok) {
-      alert("에러발생");
-    }
-
-    const data = await response.json();
-    const loadedMeal = [];
-
-    for (const meal in data) {
-      loadedMeal.push({
-        id: data[meal].id,
-        name: data[meal].name,
-        description: data[meal].description,
-        price: data[meal].price,
-      });
-    }
-
-    setMealsArray(loadedMeal);
     setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(
+        "https://reatc-http-default-rtdb.firebaseio.com/meals.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("에러가 발생함");
+      }
+
+      const data = await response.json();
+      const loadedMeal = [];
+
+      for (const meal in data) {
+        loadedMeal.push({
+          id: data[meal].id,
+          name: data[meal].name,
+          description: data[meal].description,
+          price: data[meal].price,
+        });
+      }
+      setMealsArray(loadedMeal);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -45,9 +52,21 @@ const AvailableMeals = () => {
     />
   ));
 
+  let content = <ul>{mealsList}</ul>;
+
+  if (mealsArray.length === 0) {
+    content = <p>불러올 음식이 없습니다.</p>;
+  }
+  if (error) {
+    content = <p>에러가 발생했음</p>;
+  }
+  if (isLoading) {
+    content = <p>Loading.....</p>;
+  }
+
   return (
     <section className={classes.meals}>
-      <Card>{!isLoading ? <p>Loading.....</p> : <ul>{mealsList}</ul>}</Card>
+      <Card>{content}</Card>
     </section>
   );
 };
