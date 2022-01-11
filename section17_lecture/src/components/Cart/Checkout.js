454cc000fe9baa6e3a95c06ba1e501483c8615df
Won-young <dnjsdud2257@gmail.com>
2022-01-11 20:52:39 +0900

    양식 데이터 가져오기 에러 처리

---
+++ section17_lecture/src/components/Cart/Checkout.js
@@ -0,0 +1,95 @@
+import { useRef, useState } from "react";
+import classes from "./Checkout.module.css";
+
+const isEmpty = (value) => value.trim() === "";
+const isFiveChars = (value) => value.trim().length === 5;
+
+const Checkout = (props) => {
+  const [formInputsValidity, setFormInputsValidity] = useState({
+    name: true,
+    street: true,
+    city: true,
+    postalCode: true,
+  });
+
+  const nameInputRef = useRef();
+  const streetInputRef = useRef();
+  const postalCodeInputRef = useRef();
+  const cityInputRef = useRef();
+
+  const confirmHandler = (event) => {
+    event.preventDefault();
+    const enteredName = nameInputRef.current.value;
+    const enteredStreet = streetInputRef.current.value;
+    const enteredPostalCode = postalCodeInputRef.current.value;
+    const enteredCity = cityInputRef.current.value;
+
+    const enteredNameValid = !isEmpty(enteredName);
+    const enteredStreetValid = !isEmpty(enteredStreet);
+    const enteredPostalCodeValid = isFiveChars(enteredPostalCode);
+    const enteredCityValid = !isEmpty(enteredCity);
+
+    setFormInputsValidity({
+      name: enteredNameValid,
+      street: enteredStreetValid,
+      city: enteredCityValid,
+      postalCode: enteredPostalCodeValid,
+    });
+
+    const formIsValid =
+      enteredNameValid &&
+      enteredCityValid &&
+      enteredPostalCodeValid &&
+      enteredStreetValid;
+
+    if (formIsValid) {
+      return;
+    }
+  };
+
+  const nameControlClasses = `${classes.control} ${
+    formInputsValidity.name ? "" : classes.invalid
+  }`;
+  const streetControlClasses = `${classes.control} ${
+    formInputsValidity.street ? "" : classes.invalid
+  }`;
+  const postalControlClasses = `${classes.control} ${
+    formInputsValidity.postalCode ? "" : classes.invalid
+  }`;
+  const cityControlClasses = `${classes.control} ${
+    formInputsValidity.city ? "" : classes.invalid
+  }`;
+
+  return (
+    <form className={classes.form} onSubmit={confirmHandler}>
+      <div className={nameControlClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" ref={nameInputRef} />
+        {!formInputsValidity.name && <p>입력하세요!</p>}
+      </div>
+      <div className={streetControlClasses}>
+        <label htmlFor="street">Street</label>
+        <input type="text" id="street" ref={streetInputRef} />
+        {!formInputsValidity.street && <p>입력하세요!</p>}
+      </div>
+      <div className={postalControlClasses}>
+        <label htmlFor="postal">Postal Code</label>
+        <input type="text" id="postal" ref={postalCodeInputRef} />
+        {!formInputsValidity.postalCode && <p>입력하세요!</p>}
+      </div>
+      <div className={cityControlClasses}>
+        <label htmlFor="city">City</label>
+        <input type="text" id="city" ref={cityInputRef} />
+        {!formInputsValidity.city && <p>입력하세요!</p>}
+      </div>
+      <div className={classes.actions}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
+    </form>
+  );
+};
+
+export default Checkout;
