8e5f7c3269d762a822b9c3d47d38317c72094662
Won-young <dnjsdud2257@gmail.com>
2022-01-13 13:40:37 +0900

    로그인 화면 redux 연동

--- section18_02/src/App.js
+++ section18_02/src/App.js
@@ -3,11 +3,15 @@ import Counter from "./components/Counter";
 import Header from "./components/Header";
 import Auth from "./components/Auth";
 
+import { useSelector } from "react-redux";
+
 function App() {
+  const auth = useSelector((state) => state.auth.isAuthenticated);
+
   return (
     <Fragment>
       <Header />
-      <Auth />
+      {!auth && <Auth />}
       <Counter />
     </Fragment>
   );
